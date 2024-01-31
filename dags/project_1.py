21632eaa7813b6f9e916a81aad7b7ed8aaafdaa3
qinqizhang <qinqi@udel.edu>
2024-01-31 18:18:08 -0500

    update

---
+++ dags/project_1.py
@@ -0,0 +1,46 @@
+"""
+Example use of Snowflake related operators.
+"""
+import os
+from datetime import datetime
+
+from airflow import DAG
+from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
+from airflow.providers.snowflake.transfers.s3_to_snowflake import S3ToSnowflakeOperator
+
+
+SNOWFLAKE_CONN_ID = 'snowflake_conn'
+SNOWFLAKE_DATABASE = 'AIRFLOW0124'
+SNOWFLAKE_SCHEMA = 'BF_DEV'
+
+SNOWFLAKE_ROLE = 'BF_DEVELOPER0124'
+SNOWFLAKE_WAREHOUSE = 'BF_ETL0124'
+
+SNOWFLAKE_STAGE = 'S3_STAGE_TRANS_ORDER' 
+
+
+with DAG(
+    "group5_project1",
+    start_date=datetime(2022, 11, 28),
+    schedule_interval='0 7 * * *',
+    default_args={'snowflake_conn_id': SNOWFLAKE_CONN_ID},
+    tags=['beaconfire_group5'], 
+    catchup=True,
+) as dag:
+
+    copy_into_prestg = S3ToSnowflakeOperator(
+        task_id='prestg_s3_to_sn',
+        s3_keys=['product_order_trans_{{ ds[5:7]+ds[8:10]+ds[0:4] }}.csv'],
+        table='prestage_TableName_Group5',
+        schema=SNOWFLAKE_SCHEMA,
+        stage=SNOWFLAKE_STAGE,
+        file_format='''(type = 'CSV', field_delimiter = ',', SKIP_HEADER = 1 \
+            NULL_IF =('NULL','null',''), empty_field_as_null = true, FIELD_OPTIONALLY_ENCLOSED_BY = '\"' \
+            ESCAPE_UNENCLOSED_FIELD = NONE RECORD_DELIMITER = '\n')''',
+    )
+
+    copy_into_prestg
+          
+        
+    
+

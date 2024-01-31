56e18fbef9314f454fb2055e0497debd64e3c579
ZIQIUSHAO <zs256@georgetown.edu>
2024-01-31 18:10:16 -0500

    first try

---
+++ s3_data_copy_group1.py
@@ -0,0 +1,46 @@
+"""
+Example use of Snowflake related operators.
+"""
+import os
+from datetime import datetime
+
+from airflow import DAG
+from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
+from airflow.providers.snowflake.transfers.s3_to_snowflake import S3ToSnowflakeOperator
+
+
+SNOWFLAKE_CONN_ID = 'snowflake_conn'
+SNOWFLAKE_DATABASE = 'AIRFLOW0124'
+SNOWFLAKE_SCHEMA = 'BF_DEV'
+
+SNOWFLAKE_ROLE = 'BF_DEVELOPER0124'
+SNOWFLAKE_WAREHOUSE = 'BF_ETL0124'
+
+SNOWFLAKE_STAGE = 's3_stage_trans_order'
+#S3_FILE_PATH = 'ProductOrderTrans_Group1_20240130.csv'
+
+with DAG(
+    "s3_data_copy_group1",
+    start_date=datetime(2024, 01, 30),
+    schedule_interval='0 7 * * *',
+    default_args={'snowflake_conn_id': SNOWFLAKE_CONN_ID},
+    tags=['beaconfire'],
+    catchup=True,
+) as dag:
+
+    copy_into_prestg = S3ToSnowflakeOperator(
+        task_id='prestg_product_order_trans',
+        s3_keys=['ProductOrderTrans_Group1_{{ ds[0:4]+ds[5:7]+ds[8:10] }}.csv'],
+        table='PRESTAGE_PRODUCT_ORDER_TRANS_Group1',
+        schema=SNOWFLAKE_SCHEMA,
+        stage=SNOWFLAKE_STAGE,
+        file_format='''(type = 'CSV', field_delimiter = ',', SKIP_HEADER = 1 \
+            NULL_IF =('NULL','null',''), empty_field_as_null = true, FIELD_OPTIONALLY_ENCLOSED_BY = '\"' \
+            ESCAPE_UNENCLOSED_FIELD = NONE RECORD_DELIMITER = '\n')''',
+    )
+
+    copy_into_prestg
+          
+        
+    
+

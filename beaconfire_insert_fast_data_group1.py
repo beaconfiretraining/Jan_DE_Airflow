21cfdfc47f07ca1bf9ec20fef62391d211ef4e86
huiyehuang <89879318+huiyehuang@users.noreply.github.com>
2024-01-31 22:04:04 -0500

    Add files via upload

---
+++ beaconfire_insert_fast_data_group1.py
@@ -0,0 +1,53 @@
+"""
+Example use of Snowflake related operators.
+"""
+import os
+from datetime import datetime
+from airflow import DAG
+from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
+
+
+
+SNOWFLAKE_CONN_ID = 'snowflake_conn'
+SNOWFLAKE_DATABASE = 'AIRFLOW0124'
+SNOWFLAKE_SCHEMA = 'BF_DEV'
+
+SNOWFLAKE_ROLE = 'BF_DEVELOPER0124'
+SNOWFLAKE_WAREHOUSE = 'BF_ETL0124'
+#SNOWFLAKE_STAGE = 'beaconfire_stage'
+
+
+#SNOWFLAKE_SAMPLE_TABLE1 = 'DIM_COMPANY_GROUP1'
+SNOWFLAKE_SAMPLE_TABLE = 'FACT_STOCK_HISTORY_GROUP1'
+# SQL commands
+
+
+DAG_ID = "beaconfire_dev_db_group1"
+# [START howto_operator_snowflake]
+
+with DAG(
+    DAG_ID,
+    start_date=datetime(2024, 1, 30),
+    schedule_interval='0 7 * * *',
+    default_args={'snowflake_conn_id': SNOWFLAKE_CONN_ID},
+    tags=['beaconfire'],
+    catchup=True,
+) as dag:
+    
+    snowflake_insert_fact_group1 = SnowflakeOperator(
+       task_id='snowflake_insert_fact_group1',
+       sql='fact_table_insert_date_group1.sql',
+       split_statements=True,
+       warehouse=SNOWFLAKE_WAREHOUSE,
+        database=SNOWFLAKE_DATABASE,
+        schema=SNOWFLAKE_SCHEMA,
+        role=SNOWFLAKE_ROLE
+    )
+
+    # [END howto_operator_snowflake]
+
+    (
+            snowflake_insert_fact_group1     
+    )
+    # [END snowflake_example_dag]
+

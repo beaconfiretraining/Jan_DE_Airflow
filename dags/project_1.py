0ee38001db374f5f91764db3e7b0fa310e4eaa5f
Angelica Zhang <angelicayidan@gmail.com>
2024-01-31 18:14:06 -0800

    Update project_1.py

--- dags/project_1.py
+++ dags/project_1.py
@@ -19,11 +19,11 @@ SNOWFLAKE_SCHEMA = 'BF_DEV'
 SNOWFLAKE_ROLE = 'BF_DEVELOPER0124'
 SNOWFLAKE_WAREHOUSE = 'BF_ETL0124'
 
-SNOWFLAKE_STAGE = 'S3_STAGE_TRANS_ORDER'
-#S3_FILE_PATH = 'product_order_trans_group2_01302024.csv'
+SNOWFLAKE_STAGE = 'S3_STAGE_TRANS_ORDER' 
+# S3_FILE_PATH = 'transactions_group4_20240130.csv'
 
 with DAG(
-    "s3_to_snowflake_group2",
+    "s3_to_snowflake_group2_test",
     start_date=datetime(2022, 11, 28),
     schedule_interval='0 6 * * *', # set to 6AM
     default_args={'snowflake_conn_id': SNOWFLAKE_CONN_ID},
@@ -33,6 +33,10 @@ with DAG(
 
     copy_into_prestg = S3ToSnowflakeOperator(
         task_id='prestg_product_order_trans_group2', 
+
+        # run this to add previous data
+        # s3_keys=[ 'transactions_group4_20240130.csv' ],
+        # s3_keys=[ 'transactions_group4_20240131.csv' ],
 
         # then run this make the pipeline keep running when new data coming
         s3_keys=['transactions_group4_{{ ds[0:4]+ds[5:7]+ds[8:10] }}.csv'],

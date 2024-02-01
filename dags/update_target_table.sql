57d5edd0f1ae55b8b1b7ae6c33609f30b06e9f98
Yumeng Li <yumengli@Silver-Heart.local>
2024-02-01 13:11:46 -0500

    try dim symbol table

--- dags/update_target_table.sql
+++ dags/update_target_table.sql
@@ -16,18 +16,29 @@
 
 
 
-merge into FACT_STOCK_HISTORY_GROUP3 t
-using SOURCE_STOCK_HISTORY_G3 s
+-- merge into FACT_STOCK_HISTORY_GROUP3 t
+-- using SOURCE_STOCK_HISTORY_G3 s
+-- on t.SYMBOL = s.SYMBOL
+-- when matched and s.DATE <> t.DATE then
+-- update set t.DATE = s.DATE, t.OPEN = s.OPEN, t.HIGH = s.HIGH,
+-- t.LOW = s.LOW, t.CLOSE = s.CLOSE, t.VOLUME = s.VOLUME,t.ADJCLOSE =s.ADJCLOSE
+
+-- when not matched
+-- then 
+-- insert (SYMBOL,DATE,OPEN,HIGH,LOW,CLOSE,VOLUME,ADJCLOSE)
+-- values (s.SYMBOL,s.DATE,s.OPEN,s.HIGH,s.LOW,s.CLOSE,s.VOLUME,s.ADJCLOSE);
+
+merge into DIM_SYMBOLS_GROUP3 t
+using SOURCE_SYMBOLS_G3 s
 on t.SYMBOL = s.SYMBOL
-when matched and s.DATE <> t.DATE then
-update set t.DATE = s.DATE, t.OPEN = s.OPEN, t.HIGH = s.HIGH,
-t.LOW = s.LOW, t.CLOSE = s.CLOSE, t.VOLUME = s.VOLUME,t.ADJCLOSE =s.ADJCLOSE
+
+when matched and concat(s.NAME, '|', s.EXCHANGE)<> concat(t.NAME, '|', t.EXCHANGE) then
+
+update set t.NAME = s.NAME, t.EXCHANGE = s.EXCHANGE
 
 when not matched
 then 
-insert (SYMBOL,DATE,OPEN,HIGH,LOW,CLOSE,VOLUME,ADJCLOSE)
-values (s.SYMBOL,s.DATE,s.OPEN,s.HIGH,s.LOW,s.CLOSE,s.VOLUME,s.ADJCLOSE);
-
-
+insert (SYMBOL,NAME,EXCHANGE)
+values (s.SYMBOL,s.NAME,s.EXCHANGE);
 
 

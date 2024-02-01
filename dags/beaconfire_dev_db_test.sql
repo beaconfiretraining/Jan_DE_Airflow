28f021603378ff0a383a8a10c4268a3d4d88a153
Yumeng Li <yumengli@Silver-Heart.local>
2024-02-01 11:37:25 -0500

    add insert new column

--- dags/beaconfire_dev_db_test.sql
+++ dags/beaconfire_dev_db_test.sql
@@ -1,23 +1,3 @@
--- drop if exists fact_stock_history_test_group3;
--- create or replace table fact_stock_history_test_group3 as
--- (select * from "US_STOCK_DAILY"."DCCM"."STOCK_HISTORY");
--- alter table fact_stock_history_group3
--- add primary key (symbol);
-
--- drop if exists dim_symbols_group3;
--- create or replace table dim_symbols_group3 as
--- (select * from "US_STOCK_DAILY"."DCCM"."SYMBOLS");
--- alter table dim_symbols_group3
--- add primary key (symbol);
--- -- foreign key (symbol) references fact_stock_history_group3(symbol);
-
--- drop if exists dim_company_profile_group3;
--- create or replace table dim_company_profile_group3 as
--- (select * from "US_STOCK_DAILY"."DCCM"."COMPANY_PROFILE");
--- alter table dim_company_profile_group3
--- add primary key (ID);
--- -- foreign key (symbol) references fact_stock_history_group3(symbol);
-
 create or replace table fact_stock_history_group3(
     SYMBOL  VARCHAR(16) PRIMARY KEY,
     DATE  DATE,
@@ -28,7 +8,7 @@ create or replace table fact_stock_history_group3(
     VOLUME  NUMBER(38,8),
     ADJCLOSE  NUMBER(18,8)
 );
-insert into fact_stock_history_test_group3
+insert into fact_stock_history_group3
 select *
 from "US_STOCK_DAILY"."DCCM"."STOCK_HISTORY";
 
@@ -66,3 +46,9 @@ create or replace table dim_company_profile_group3(
 insert into dim_company_profile_group3
 select *
 from "US_STOCK_DAILY"."DCCM"."COMPANY_PROFILE";
+
+alter table  fact_stock_history_group3
+add MODDATE DATE;
+
+INSERT INTO fact_stock_history_group3 (MODDATE)
+VALUES (sysdate());

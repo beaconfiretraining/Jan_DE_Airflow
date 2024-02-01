dc2c70f3ac59b82c6c77101e23a7185c5ef41b52
Yumeng Li <yumengli@Silver-Heart.local>
2024-02-01 12:56:08 -0500

    try update fact table p2

--- dags/update_target_table.sql
+++ dags/update_target_table.sql
@@ -1,13 +1,13 @@
 -- update target table
 
-merge into fact_stock_history_group3 t
+merge into FACT_STOCK_HISTORY_GROUP3 t
 using SOURCE_STOCK_HISTORY_G3 s
 on t.SYMBOL = s.SYMBOL
 when matched and concat(s.DATE,'|', s.OPEN,'|',s.HIGH,'|',s.LOW,'|', s.CLOSE,'|',s.VOLUME,'|',
 s.ADJCLOSE) <> concat(t.DATE,'|', t.OPEN,'|',t.HIGH,'|',t.LOW,'|', t.CLOSE,'|',t.VOLUME,'|',
 t.ADJCLOSE) then
-update set s.DATE = t.DATE, s.OPEN = t.OPEN, s.HIGH = t.HIGH,
-s.LOW = t.LOW, s.CLOSE = t.CLOSE, s.VOLUME = t.VOLUME,s.ADJCLOSE =t.ADJCLOSE
+update set t.DATE = s.DATE, t.OPEN = s.OPEN, t.HIGH = s.HIGH,
+t.LOW = s.LOW, t.CLOSE = s.CLOSE, t.VOLUME = s.VOLUME,t.ADJCLOSE =s.ADJCLOSE
 
 when not matched
 then 

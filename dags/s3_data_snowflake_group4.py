3fd8322a4d29179370044d3877ab8aaea25aad8e
ctg3cm <ctg3cm@bu.edu>
2024-01-31 20:07:50 -0500

    Added project 1 file version 8

--- dags/s3_data_snowflake_group4.py
+++ dags/s3_data_snowflake_group4.py
@@ -31,8 +31,8 @@ with DAG(
         task_id='PRESTAGE_TRANSACTIONS_GROUP4', 
 
         # run this to add previous data
-        s3_keys=[ 'transactions_group4_20240130.csv' ],
-        # s3_keys=[ 'transactions_group4_20240131.csv' ],
+        # s3_keys=[ 'transactions_group4_20240130.csv' ],
+        s3_keys=[ 'transactions_group4_20240131.csv' ],
 
         # then run this make the pipeline keep running when new data coming
         # s3_keys=['transactions_group4_{{ ds[0:4]+ds[5:7]+ds[8:10] }}.csv'],

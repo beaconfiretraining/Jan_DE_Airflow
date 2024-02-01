eb205cde851c452cfa258de445b719e5192c0a51
Yumeng Li <yumengli@Silver-Heart.local>
2024-02-01 10:18:15 -0500

    project 2 add drop if exists

--- dags/beaconfire_dev_db_test.sql
+++ dags/beaconfire_dev_db_test.sql
@@ -1,16 +1,17 @@
+drop if exists fact_stock_history_group3;
 create or replace table fact_stock_history_group3 as
 (select * from "US_STOCK_DAILY"."DCCM"."STOCK_HISTORY");
 alter table fact_stock_history_group3
 add primary key (symbol);
 
-
+drop if exists dim_symbols_group3;
 create or replace table dim_symbols_group3 as
 (select * from "US_STOCK_DAILY"."DCCM"."SYMBOLS");
 alter table dim_symbols_group3
 add primary key (symbol);
 -- foreign key (symbol) references fact_stock_history_group3(symbol);
 
-
+drop if exists dim_company_profile_group3;
 create or replace table dim_company_profile_group3 as
 (select * from "US_STOCK_DAILY"."DCCM"."COMPANY_PROFILE");
 alter table dim_company_profile_group3

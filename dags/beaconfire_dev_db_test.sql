72f37c624322354f2138f002d7440ae1bf89246e
Yumeng Li <yumengli@Silver-Heart.local>
2024-01-31 22:07:11 -0500

    remove foreign key project 2

--- dags/beaconfire_dev_db_test.sql
+++ dags/beaconfire_dev_db_test.sql
@@ -7,12 +7,12 @@ add primary key (symbol);
 create or replace table dim_symbols_group3 as
 (select * from "US_STOCK_DAILY"."DCCM"."SYMBOLS");
 alter table dim_symbols_group3
-add primary key (symbol)
-foreign key (symbol) references fact_stock_history_group3(symbol);
+add primary key (symbol);
+-- foreign key (symbol) references fact_stock_history_group3(symbol);
 
 
 create or replace table dim_company_profile_group3 as
 (select * from "US_STOCK_DAILY"."DCCM"."COMPANY_PROFILE");
 alter table dim_company_profile_group3
-add primary key (ID)
-foreign key (symbol) references fact_stock_history_group3(symbol);;
+add primary key (ID);
+-- foreign key (symbol) references fact_stock_history_group3(symbol);

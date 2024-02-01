c7709f6f3c2b50f43be972d3b89c5348c65a9a0e
Angelica Zhang <angelicayidan@gmail.com>
2024-01-31 17:29:27 -0800

    Update Project 1.py

--- dags/Project 1.py
+++ dags/Project 1.py
@@ -25,19 +25,19 @@ SNOWFLAKE_STAGE = 'S3_STAGE_TRANS_ORDER'
 with DAG(
     "s3_to_snowflake_group2",
     start_date=datetime(2022, 11, 28),
-    schedule_interval='0 17 * * *', #next run 01/31/17:00
+    schedule_interval='0 6 * * *', # set to 6AM
     default_args={'snowflake_conn_id': SNOWFLAKE_CONN_ID},
     tags=['beaconfire'],
-    catchup=False,
+    catchup=True, # keep running
 ) as dag:
 
     copy_into_prestg = S3ToSnowflakeOperator(
-        task_id='prestg_product_order_trans_group2',
-        #s3_keys=['product_order_trans_group2_{{ ds[5:7]+ds[8:10]+ds[0:4] }}.csv'],
-        s3_keys=['product_order_trans_group2_20240130.csv',
-                 'product_order_trans_group2_20240131.csv',
-                 'product_order_trans_group2_20240201.csv'],
-        table='prestg_product_order_trans_group2',
+        task_id='prestg_product_order_trans_group2', 
+
+        # then run this make the pipeline keep running when new data coming
+        s3_keys=['product_order_trans_group2_{{ ds[0:4]+ds[5:7]+ds[8:10] }}.csv'],
+
+        table='prestg_product_order_trans_group2', 
         schema=SNOWFLAKE_SCHEMA,
         stage=SNOWFLAKE_STAGE,
         file_format='''(type = 'CSV', field_delimiter = ',', SKIP_HEADER = 1 \

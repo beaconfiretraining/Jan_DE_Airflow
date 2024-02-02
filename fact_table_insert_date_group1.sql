d9026389b566bfb8d0f9e24891bc72ba6cea1cbb
tb0199322 <157724824+tb0199322@users.noreply.github.com>
2024-02-01 22:07:22 -0500

    Update fact_table_insert_date_group1.sql

--- fact_table_insert_date_group1.sql
+++ fact_table_insert_date_group1.sql
@@ -1,18 +1,7 @@
-MERGE INTO FACT_STOCK_HISTORY_GROUP1 AS target
-USING (
-  SELECT
-    SYMBOL,
-    DATE,
-    OPEN,
-    HIGH,
-    LOW,
-    CLOSE,
-    VOLUME,
-    ADJCLOSE,
-    ROW_NUMBER() OVER(PARTITION BY SYMBOL, DATE ORDER BY (SELECT NULL)) as rn -- Arbitrarily ordering
-  FROM US_STOCK_DAILY.DCCM.STOCK_HISTORY
-) AS source
-ON target.SYMBOL = source.SYMBOL AND target.date = source.date
+MERGE INTO AIRFLOW0124.BF_DEV.FACT_STOCK_HISTORY AS target
+USING US_STOCK_DAILY.DCCM.STOCK_HISTORY AS source
+ON target.SYMBOL = source.SYMBOL
+and target.date = source.date
   /*
 WHEN MATCHED AND (
   target.open <> source.open OR
@@ -21,16 +10,17 @@ WHEN MATCHED AND (
   target.close <> source.close OR
   target.volume <> source.volume OR
   target.adjclose <> source.adjclose
-) THEN UPDATE SET
-  target.open = source.open,
-  target.high = source.high,
-  target.low = source.low,
-  target.close = source.close,
-  target.volume = source.volume,
-  target.adjclose = source.adjclose
+  -- Add other columns as needed
+) THEN
+  UPDATE SET
+    target.open = source.open,
+    target.high = source.high,
+    target.low = source.low,
+    target.close = source.close,
+    target.volume = source.volume,
+    target.adjclose = source.adjclose
+    -- Update other columns as needed
 */
-WHEN NOT MATCHED AND source.rn = 1 THEN INSERT (
-  SYMBOL, DATE, OPEN, HIGH, LOW, CLOSE, VOLUME, ADJCLOSE
-) VALUES (
-  source.SYMBOL, source.DATE, source.OPEN, source.HIGH, source.LOW, source.CLOSE, source.VOLUME, source.ADJCLOSE
-);
+WHEN NOT MATCHED THEN
+  INSERT (SYMBOL, DATE, OPEN, HIGH, LOW, CLOSE, VOLUME, ADJCLOSE)
+  VALUES (source.SYMBOL, source.DATE, source.OPEN, source.HIGH, source.LOW, source.CLOSE, source.VOLUME, source.ADJCLOSE);

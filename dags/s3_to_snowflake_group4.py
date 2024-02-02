733d518b6440287f73ba439f28fe3838e26eb99b
ctg3cm <ctg3cm@bu.edu>
2024-02-02 10:55:41 -0500

    Added project 1 file version 12

--- dags/s3_to_snowflake_group4.py
+++ dags/s3_to_snowflake_group4.py
@@ -21,7 +21,7 @@ SNOWFLAKE_STAGE = 'S3_STAGE_TRANS_ORDER'
 with DAG(
     "s3_to_snowflake_group4", # name
     start_date=datetime(2024, 1, 30),
-    schedule_interval='53 15 * * *', # set to 15:53 UTC
+    schedule_interval='57 15 * * *', # set to 15:57 UTC
     default_args={'snowflake_conn_id': SNOWFLAKE_CONN_ID},
     tags=['beaconfire'],
     catchup=True, # keep running
